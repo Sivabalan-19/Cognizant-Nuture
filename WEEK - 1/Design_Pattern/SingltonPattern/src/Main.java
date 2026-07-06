27460e99586c73e7d4ff0b2315fc7843125bb5b0
Sivabalan-19 <pmsiva.1906@gmail.com>
2026-07-06 21:58:57 +0530

    Singleton class completed

---
+++ WEEK - 1/Design_Pattern/SingltonPattern/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger object is created.");
+        } else {
+            System.out.println("More than one Logger object is created.");
+        }
+    }
+}
