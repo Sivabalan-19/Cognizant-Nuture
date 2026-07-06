77d3189c6434dc31de46ce0a2a487e6962f602d4
Sivabalan-19 <pmsiva.1906@gmail.com>
2026-07-06 22:44:59 +0530

    mockito exerise 2 added

---
+++ WEEK - 3/mockito/Exercise-2/src/test/java/MyServiceTest.java
@@ -0,0 +1,17 @@
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+
+public class MyServiceTest {
+
+    @Test
+    void testVerifyInteraction() {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
